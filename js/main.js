b75784f9a66b705a006a6b10cf423309f2249ae5
yurison <yu_ri_son@naver.com>
2023-12-10 15:58:27 +0900

    topToScroll button

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener("blur", function () {
 });
 
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
 
 window.addEventListener(
   "scroll",
@@ -25,14 +26,26 @@ window.addEventListener(
         opacity: 0,
         display: "none",
       });
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: "block",
       });
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
     }
   }, 300)
 );
+
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 
